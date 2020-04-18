527cbdb60048f3f074239b60775b0c01287a6ae4
G-A-O <626614916@qq.com>
2020-04-18 16:56:47 +0800

    shiro搭建

---
+++ shirodemo/src/main/java/cn/loveyx815/shirodemo/ShirodemoApplication.java
@@ -0,0 +1,13 @@
+package cn.loveyx815.shirodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirodemoApplication.class, args);
+    }
+
+}
